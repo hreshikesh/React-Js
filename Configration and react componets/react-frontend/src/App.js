faaaf0853c7773361ccc46215909026620844583
Hreshikesh <ailhreshikesh@gmail.com>
2025-11-06 13:31:27 +0530

    using bootstrap,states and props in react

--- Configration and react componets/react-frontend/src/App.js
+++ Configration and react componets/react-frontend/src/App.js
@@ -2,11 +2,12 @@
 import './App.css';
 import MyMessage from './Message';
 import { useState } from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function MySampleComponemt(){
   return(
-    <div>
-      <button>This is a sample component</button>
+    <div className='container-fluid justify-content-center text-center mt-5'>
+      <button type='submit' className='btn btn-success p-3'>This is a sample component</button>
     </div>
   )
 }
@@ -22,16 +23,32 @@ function Welcome(props){
 
 
 
-function Counter(){
-  const [count, setCount] = useState(0);
+function Counter(props){
+  const [count, setCount] = useState(props.initalValue);
+  return(
+    <div className='row container-fluid d-flex justify-content-center text-center mt-5'>
+    <div class="card w-25 bg-success-subtle" >
+  <div class="card-body">
+    <h5 class="card-title">Counter App for {props.name}</h5>
+    <h3>count:{count}</h3>
+      <button onClick={function(){setCount(count+1)}} className='btn btn-outline-warning'>increase count</button>
+      <button onClick={function(){setCount(count-1)}}className='btn btn-outline-danger'>decrease count</button>
+  </div>
+   </div>
+   </div>
+  )
+}
+
+function InputApp(){
+  const[input,setInput]=useState("");
   return(
     <div>
-      <h1>Counter App</h1>
-      <h3>count:{count}</h3>
-      <button onClick={function(){setCount(count+1)}}>increase count</button>
-      <button onClick={function(){setCount(count-1)}}>decrease count</button>
+      <h1>Input name</h1>
+      <input type="text" value={input} onInput={function(e){setInput(e.target.value)}} / >
+      <h2>Your name is:{input}</h2>
     </div>
   )
+
 }
 
 export default function myFunction(){
@@ -41,7 +58,9 @@ export default function myFunction(){
     <Welcome name="Rishi" age="25"/>
     <Welcome name="Ajay" age="24"/>
     <MyMessage />
-    <Counter/>
+    <Counter name="Rishi" initalValue={3}/>
+    <Counter name="Ajay" initalValue={7}/>
+    <InputApp />
     </>
   );
 }
