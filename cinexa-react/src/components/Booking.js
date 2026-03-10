12bbe9f5bf80a1494bd89b2d68166d0bb417ae96
Hreshikesh <ailhreshikesh@gmail.com>
2026-03-10 13:24:34 +0530

    booking page

--- cinexa-react/src/components/Booking.js
+++ cinexa-react/src/components/Booking.js
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 const Booking = () => {
     const { movieId } = useParams();
@@ -14,9 +15,12 @@ const Booking = () => {
     const [luxurySeats, setLuxurySeats] = useState([]);
     const [regularSeats, setRegularSeats] = useState([]);
     const [generalSeats, setGeneralSeats] = useState([]);
+    const [userEmail, setUserEmail] = useState("");
+    const [selectedSeats, setSelectedSeats] = useState([]);
+    const [totalPrice, setTotalPrice] = useState(0);
+    const [fetchedSeat,setFetchedSeats]=useState([]);
 
 
-    
 
     useEffect(() => {
         axios
@@ -25,12 +29,31 @@ const Booking = () => {
             .catch((err) => setError(err.message));
     }, [movieId]);
 
-
-
-
-    const handleSubmit=(e)=>{
-        e.preventDefault();
+    const fetchBookedSeats=()=>{
+        axios.get(`http://localhost:8080/api/booking/getMovieById/getAllBookedSeats/${bookingDate}/${movieId}`)
+        .then((response)=>setFetchedSeats(response.data))
+        .catch((err)=>setError(err.message))
     }
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const data = {
+            movieId: movieId,
+            userEmail: userEmail,
+            bookingDate: bookingDate,
+            selectedSeats: selectedSeats,
+            price: totalPrice
+        };
+        axios.post("http://localhost:8080/api/booking/createBooking", data)
+            .then((res) => {
+                setStep(3);
+            })
+            .catch((err) => {
+                setError(err.message);
+            });
+    };
 
     const fetchSeats = () => {
         axios.get("http://localhost:8080/api/seat/getSeat")
@@ -43,7 +66,7 @@ const Booking = () => {
         if (seats.length > 0) {
             SeatFilter();
         }
-    },[seats]);
+    }, [seats]);
 
     const validateDate = (date) => {
         const selectedDate = new Date(date);
@@ -60,6 +83,8 @@ const Booking = () => {
         setDateError("");
         setStep(2);
         fetchSeats();
+        fetchBookedSeats();
+
     };
 
 
@@ -75,9 +100,6 @@ const Booking = () => {
     }
 
 
-    const seatPrice={
-
-    }
 
 
 
@@ -115,108 +137,150 @@ const Booking = () => {
                     </div>
                 </div>
 
+                <form onSubmit={handleSubmit}>
+                    {step === 1 && (
+                        <div className="space-y-4">
+                            <label className="block text-lg">UserEmail</label>
+                            <input
+                                type="email"
+                                value={userEmail}
+                                required
+                                onChange={(e) => setUserEmail(e.target.value)}
+                                className="w-full p-3 rounded-xl bg-white/20 border border-white/30 text-white"
+                            />
+                            <label className="block text-lg">Select Date</label>
+                            <input
+                                type="date"
+                                value={bookingDate}
+                                onChange={(e) => setBookingDate(e.target.value)}
+                                className="w-full p-3 rounded-xl bg-white/20 border border-white/30 text-white"
+                            />
+                            {dateError && (
+                                <p className="text-red-400">{dateError}</p>
+                            )}
 
-                {step === 1 && (
-                    <div className="space-y-4">
-                        <label className="block text-lg">Select Date</label>
-                        <input
-                            type="date"
-                            value={bookingDate}
-                            onChange={(e) => setBookingDate(e.target.value)}
-                            className="w-full p-3 rounded-xl bg-white/20 border border-white/30 text-white"
-                        />
-                        {dateError && (
-                            <p className="text-red-400">{dateError}</p>
-                        )}
-
-                        <button
-                            type="button"
-                            onClick={handleNext}
-                            className="w-full mt-4 py-3 rounded-xl bg-blue-600 hover:bg-blue-700 transition"
-                        >
-                            Next
-                        </button>
-                    </div>
-                )}
+                            <button
+                                type="button"
+                                onClick={handleNext}
+                                className="w-full mt-4 py-3 rounded-xl bg-blue-600 hover:bg-blue-700 transition"
+                            >
+                                Next
+                            </button>
+                        </div>
+                    )}
 
 
-                {step === 2 && (
-                    <div className="space-y-6">
-                        <h2 className="text-xl font-semibold text-center">
-                            Select Your Seat
-                        </h2>
-
+                    {step === 2 && (
                         <div className="space-y-6">
-                            <div className="space-y-4">
-                                <div className="flex gap-3">
-                                    <h3 className="text-lg font-semibold mb-2">General Seats</h3>
-                                    <span className="text-emerald-600">Price:{movieDetails.moviePrice}</span>
-                                </div>
-                                <div className="flex flex-wrap justify-center gap-6">
-                                    {generalSeats.map((seat) => (
-                                        <button
-                                            key={seat.id}
-                                            type="button"
-                                            className={`w-14 h-14 rounded-lg bg-white/20 ${seat.seatStatus==="BOOKED" ? "bg-red-500 cursor-not-allowed" : "hover:bg-green-500"} transition`}
-                                        >
-                                            {seat.seatName}
-                                        </button>
+                            <h2 className="text-xl font-semibold text-center">
+                                Select Your Seat
+                            </h2>
+
+                            <div>
+                                <h3 className="text-lg font-semibold mb-2">Selected Seats:</h3>
+                                <div className="flex flex-wrap gap-4">
+                                    {selectedSeats.map((seat, index) => (
+                                        <span key={index} className="px-3 py-1 bg-blue-600 rounded-full">
+                                            {seat}
+                                        </span>
                                     ))}
+                                </div>
+                                <div className="mt-4">
+                                    <h3 className="text-lg font-semibold">Total Price: <span className="text-emerald-400">{totalPrice}</span></h3>
                                 </div>
                             </div>
-                            <div>
-                                <div className="flex gap-3">
-                                    <h3 className="text-lg font-semibold mb-2">Regular Seats</h3>
-                                    <span className="text-emerald-600">Price:{movieDetails.moviePrice + 50}</span>
+                            <div className="space-y-6">
+                                <div className="space-y-4">
+                                    <div className="flex gap-3">
+
+                                        <h3 className="text-lg font-semibold mb-2">General Seats</h3>
+                                        <span className="text-emerald-600">Price:{movieDetails.moviePrice}</span>
+                                    </div>
+                                    <div className="flex flex-wrap justify-center gap-6">
+                                        {generalSeats.map((seat) => (
+                                            <button
+                                                key={seat.id}
+                                                type="button"
+                                                disabled={selectedSeats.includes(seat.seatName)}
+                                                className={`w-14 h-14 rounded-lg bg-white/20 ${selectedSeats.includes(seat.seatName) ? "disabled:bg-emerald-500 cursor-not-allowed outline-purple-100" : "hover:bg-green-500"} ${seats.includes(fetchedSeat) ? "bg-red-700" :"hover:bg-green-500" } transition`}
+                                                onClick={() => { setSelectedSeats([...selectedSeats, seat.seatName]); setTotalPrice(totalPrice + movieDetails.moviePrice + 50); }}
+                                            >
+                                                {seat.seatName}
+                                            </button>
+                                        ))}
+                                    </div>
                                 </div>
-                                <div className="flex flex-wrap justify-center gap-6">
-                                    {regularSeats.map((seat) => (
-                                        <button
-                                            key={seat.id}
-                                            type="button"
-                                            className={`w-14 h-14 rounded-lg bg-white/20 ${seat.seatStatus==="BOOKED" ? "bg-red-500" : "hover:bg-green-500"} transition`}
-                                        >
-                                            {seat.seatName}
-                                        </button>
-                                    ))}
+                                <div>
+                                    <div className="flex gap-3">
+                                        <h3 className="text-lg font-semibold mb-2">Regular Seats</h3>
+                                        <span className="text-emerald-600">Price:{movieDetails.moviePrice + 50}</span>
+                                    </div>
+                                    <div className="flex flex-wrap justify-center gap-6">
+                                        {regularSeats.map((seat) => (
+                                            <button
+                                                key={seat.id}
+                                                type="button"
+                                                className={`w-14 h-14 rounded-lg bg-white/20 ${selectedSeats.includes(seat.seatName) ? "bg-emerald-500" : "hover:bg-green-500"} ${seats.includes(fetchSeats) ? "bg-red-700" :"hover:bg-green-500" } transition`}
+                                                onClick={() => { 
+                                                    setSelectedSeats([...selectedSeats, seat.seatName]); 
+                                                    setTotalPrice(totalPrice + movieDetails.moviePrice + 50); 
+                                                }}
+                                            >
+                                                {seat.seatName}
+                                            </button>
+                                        ))}
+                                    </div>
+                                </div>
+
+                                <div>
+                                    <div className="flex gap-3">
+                                        <h3 className="text-lg font-semibold mb-2">Luxury Seats</h3>
+                                        <span className="text-emerald-600">Price:{movieDetails.moviePrice + 120}</span>
+                                    </div>
+                                    <div className="flex flex-wrap justify-center gap-6">
+                                        {luxurySeats.map((seat) => (
+                                            <button
+                                                key={seat.id}
+                                                type="button"
+                                                className={`w-14 h-14 rounded-lg bg-white/20 ${selectedSeats.includes(seat.seatName) ? "bg-emerald-500" : "hover:bg-green-500"} transition`}
+                                                onClick={() => { setSelectedSeats([...selectedSeats, seat.seatName]); setTotalPrice(totalPrice + movieDetails.moviePrice + 120); }}
+                                            >
+                                                {seat.seatName}
+                                            </button>
+                                        ))}
+                                    </div>
                                 </div>
                             </div>
 
-                            <div>
-                                <div className="flex gap-3">
-                                    <h3 className="text-lg font-semibold mb-2">Luxury Seats</h3>
-                                    <span className="text-emerald-600">Price:{movieDetails.moviePrice + 120}</span>
-                                </div>
-                                <div className="flex flex-wrap justify-center gap-6">
-                                    {luxurySeats.map((seat) => (
-                                        <button
-                                            key={seat.id}
-                                            type="button"
-                                            className={`w-14 h-14 rounded-lg bg-white/20 ${seat.seatStatus==="BOOKED" ? "bg-red-500" : "hover:bg-green-500"} transition`}
-                                        >
-                                            {seat.seatName}
-                                        </button>
-                                    ))}
-                                </div>
+                            <div className="flex justify-between mt-8">
+                                <button
+                                    type="button"
+                                    onClick={() => setStep(1)}
+                                    className="px-6 py-3 rounded-xl bg-gray-500 hover:bg-gray-600"
+                                >
+                                    Previous
+                                </button>
+
+                                <button
+                                    type="submit"
+                                    className="px-6 py-3 rounded-xl bg-green-600 hover:bg-green-700"
+                                >
+                                    Confirm Booking
+                                </button>
                             </div>
                         </div>
 
-                        <div className="flex justify-between mt-8">
-                            <button
-                                type="button"
-                                onClick={() => setStep(1)}
-                                className="px-6 py-3 rounded-xl bg-gray-500 hover:bg-gray-600"
-                            >
-                                Previous
-                            </button>
+                    )}
+                </form>
 
-                            <button
-                                type="button"
-                                onClick={() => alert("Booking Confirmed!")}
-                                className="px-6 py-3 rounded-xl bg-green-600 hover:bg-green-700"
-                            >
-                                Confirm Booking
-                            </button>
+                {step === 3 && (
+                    <div className="text-center mt-8">
+                        <h2 className="text-2xl font-bold mb-4">Booking Successful!</h2>
+                        <p className="text-lg">Thank you for booking with us. Enjoy your movie!</p>
+                        <div className="mt-6">
+                            <Link to="/all-movies" className="px-6 py-3 rounded-xl bg-blue-600 hover:bg-blue-700">
+                                Book Another Movie
+                            </Link>
                         </div>
                     </div>
                 )}
